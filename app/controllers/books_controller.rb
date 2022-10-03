d480bf77d3cf866ff65749372bf15fb44e1642dc
EC2 Default User <ec2-user@ip-172-31-28-221.ec2.internal>
2022-10-03 15:38:49 +0000

    show, index追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,28 @@
 class BooksController < ApplicationController
   def index
     @book = Book.new#インスタンス生成
+    @books = Book.all
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/'
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
 
+
+
+  private
+
+  def book_params
+  params.require(:book).permit(:title, :body)#require:モデル名を小文字にしたものpermit カラム
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get '/books' => 'books#index'
-  get 'books/show'
-  get 'books/edit'
+  post 'books' => 'books#create', as: 'create_book'
+  get 'books/:id' => 'books#show', as: 'show_book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   get '/' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
