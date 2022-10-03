683b4f7e692d9597904592411f61ce6c498eeb28
EC2 Default User <ec2-user@ip-172-31-28-221.ec2.internal>
2022-10-03 17:59:58 +0000

    バリデーション機能（未完成)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,13 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to '/'
+    if book.save
+      redirect_to book_path(book.id)
+    else
+      @book = Book.new#インスタンス生成
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -15,6 +20,23 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)#引数は入力後の内容
+      redirect_to book_path(book.id)
+    else
+      @book = Book.find(params[:id])
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy#消すだけだから引数いらない
+    redirect_to books_path
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get '/books' => 'books#index'
-  post 'books' => 'books#create', as: 'create_book'
-  get 'books/:id' => 'books#show', as: 'show_book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   get '/' => 'homes#top'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
